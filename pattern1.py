674a03f6d37d15a43115e542f507b831f573b306
Felci Veronica <115362579+Felciveronica@users.noreply.github.com>
2023-09-06 11:41:22 +0530

    pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,9 +1,8 @@
 n=8
 for j in range(1,2*n):
     for i in range(1,n+1):
-        print(" ",end="")
-        if(i>abs(n-j)+1):
+        if(i>abs(n-j)):
            print("*",end="")
-       
+        print(" ",end="")
     print()
       
