9ca600d809f766cd33fc0f6c4634c5a080839264
Felci Veronica <felcivero00nica@gmail.com>
2023-12-23 19:06:23 +0530

    Update script.js

--- Module5/js/script.js
+++ Module5/js/script.js
@@ -345,3 +345,118 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
+
+
+
+
+
+
+
+
+$(function () {
+  // Event listener for handling blur on the navbar toggle button
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+(function (global) {
+  var dc = {};
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+  var categoryHtml = "snippets/category-snippet.html";
+  var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+  var menuItemsTitleHtml = "snippets/menu-items-title.html";
+  var menuItemHtml = "snippets/menu-item.html";
+
+  // Function for inserting HTML into a specified selector
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+  // Function for displaying a loading icon
+  var showLoading = function (selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
+
+  // Function for substituting a placeholder with a property value
+  var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string.replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  };
+
+  // Function to remove the 'active' class from home and switch to the menu button
+  var switchMenuToActive = function () {
+    var classes = document.querySelector("#navHomeButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    classes = document.querySelector("#navMenuButton").className;
+    if (classes.indexOf("active") === -1) {
+      classes += " active";
+      document.querySelector("#navMenuButton").className = classes;
+    }
+  };
+
+  // Event listener for the DOMContentLoaded event
+  document.addEventListener("DOMContentLoaded", function (event) {
+    // Show loading icon
+    showLoading("#main-content");
+
+    // AJAX request to get all categories from the server
+    $ajaxUtils.sendGetRequest(
+      allCategoriesUrl,
+      buildAndShowHomeHTML,
+      true
+    );
+  });
+
+  // Function to build and show the home page based on categories array
+  function buildAndShowHomeHTML(categories) {
+    // AJAX request to load home snippet page
+    $ajaxUtils.sendGetRequest(
+      homeHtmlUrl,
+      function (homeHtml) {
+        // Choose a random category
+        var chosenCategory = chooseRandomCategory(categories);
+
+        // Substitute {{randomCategoryShortName}} in the home html snippet
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategory.short_name);
+
+        // Insert the produced HTML into the main page
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      },
+      false
+    );
+  }
+
+  // Function to choose a random category from an array
+  function chooseRandomCategory(categories) {
+    var randomArrayIndex = Math.floor(Math.random() * categories.length);
+    return categories[randomArrayIndex];
+  }
+
+  // Additional functions for loading menu categories and menu items
+  // (Not shown here for brevity)
+
+  global.$dc = dc;
+
+})(window);
+
+
+
+
+
+
+
+
+
